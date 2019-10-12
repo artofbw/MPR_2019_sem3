41ebe052322485175538622538f367f0000b25cd
artofbw <artofbw@gmail.com>
2019-10-12 09:16:05 +0200

    first contoller

--- src/main/java/lukasz/ksiegarnia/demo/controller/BookContoller.java
+++ src/main/java/lukasz/ksiegarnia/demo/controller/BookContoller.java
@@ -1,4 +1,23 @@
 package lukasz.ksiegarnia.demo.controller;
 
+import lukasz.ksiegarnia.demo.model.Book;
+import lukasz.ksiegarnia.demo.service.BookService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
 public class BookContoller {
+    private final BookService bookService;
+
+    public BookContoller(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/all")
+    public List<Book> findAll() {
+        return bookService.findAll();
+    }
 }
