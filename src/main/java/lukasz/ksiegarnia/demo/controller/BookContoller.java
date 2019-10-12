2cd13c631faee5f7d3318c1cc2aa6378ec6dc775
artofbw <artofbw@gmail.com>
2019-10-12 09:23:08 +0200

    missing controller

--- src/main/java/lukasz/ksiegarnia/demo/controller/BookContoller.java
+++ src/main/java/lukasz/ksiegarnia/demo/controller/BookContoller.java
@@ -1,4 +1,24 @@
 package lukasz.ksiegarnia.demo.controller;
 
+import lukasz.ksiegarnia.demo.model.Book;
+import lukasz.ksiegarnia.demo.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
 public class BookContoller {
+    private final BookService bookService;
+
+    public BookContoller(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/all")
+    public List<Book> findAll() {
+        return bookService.findAll();
+    }
 }
